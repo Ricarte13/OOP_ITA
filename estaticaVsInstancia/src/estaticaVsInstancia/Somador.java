4446d93d706a13cff7037f4afcacd0e25b2bfde0
Vinícius Ricarte <128393426+Ricarte13@users.noreply.github.com>
2023-11-20 19:17:55 -0300

    Add files via upload

---
+++ estaticaVsInstancia/src/estaticaVsInstancia/Somador.java
@@ -0,0 +1,18 @@
+package estaticaVsInstancia;
+
+public class Somador {
+	
+	String nome;
+	int valorInstancia = 0;
+	static int valorEstatico = 0;
+	
+	void somar() {
+		valorInstancia++;
+		valorEstatico++;
+	}
+
+	void imprimir() {
+		System.out.println("O somador" + nome + ": instancia = " + valorInstancia + " e estatico = " + valorEstatico);
+	}
+	
+}
